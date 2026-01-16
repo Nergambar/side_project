7b315b0fc585ef151e651b45694b3efd8f445fbf
negambar <negambar@student.42firenze.it>
2026-01-16 18:23:13 +0100

    boh non ho capito

---
+++ 2026/dialogues/base.hpp
@@ -0,0 +1,15 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+
+#include "../utils/utils.hpp"
+#include "../player.hpp"
+
+
+
+#endif
